e22044f0d07ad82ae7a5f31580e283d6960659d6
Arvin <chiachengz0227@gmail.com>
2024-04-22 15:53:34 +0800

    fix: title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import AddGroupPriceList from './components/AgeGroupPriceList';
-import MainLogo from "./assets/main_logo.svg";
+// import MainLogo from "./assets/main_logo.svg";
 
 const Container = styled.div`
 width: 100%;
@@ -11,9 +11,20 @@ align-items: center;
 justify-content: center;
 `;
 
-const BarImage = styled.img`
-width: 256px;
-height: 256px;
+// const BarImage = styled.img`
+// width: 256px;
+// height: 256px;
+// `;
+
+const Title = styled.h1`
+font-size: 72px;
+margin-bottom: 12px;
+`;
+
+const SmallTitle = styled.h3`
+font-size: 48px;
+margin: 0;
+margin-bottom: 48px;
 `;
 
 const ConsoleContainer = styled.div`
@@ -32,7 +43,9 @@ function App() {
 
   return (
     <Container>
-      <BarImage src={MainLogo} />
+      {/* <BarImage src={MainLogo} /> */}
+      <Title>年齡計算器</Title>
+      <SmallTitle>Age Calculator</SmallTitle>
       <AddGroupPriceList result={result} setResult={setResult} onChange={result => console.log(result)} />
       <ConsoleContainer>
         <pre><code>{`result:${JSON.stringify(result, null, 2).replace(/(\[[\d,\s]+?\])/g, (match) => match.replace(/\s/g, ''))}`}</code></pre>
